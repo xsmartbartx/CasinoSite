05cfec7856952df3f96a600b33b28df68e88da0c
xsmartbartx <bw141105@gmail.com>
2025-04-05 14:25:33 +0200

    Update EducationalCard.tsx

--- client/src/components/EducationalCard.tsx
+++ client/src/components/EducationalCard.tsx
@@ -1,2 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+
+interface EducationalCardProps {
+    title: string;
+    content: string;
+    icon: string;
+    readTime: number;
+    id: number;
+    onClick?: (id: number) => void;
+    className?: string;
+}
