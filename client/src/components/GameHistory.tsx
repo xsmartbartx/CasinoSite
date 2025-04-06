ad625b565066632506ab2df433abf1f1d252acc9
xsmartbartx <bw141105@gmail.com>
2025-04-06 17:10:47 +0200

    Update GameHistory.tsx

--- client/src/components/GameHistory.tsx
+++ client/src/components/GameHistory.tsx
@@ -1,3 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { formatCurrency, formatTimestamp } from "@/lib/gameUtils";
 import { useAuth } from "@/hooks/useAuth";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+  } from "@/components/ui/table";
