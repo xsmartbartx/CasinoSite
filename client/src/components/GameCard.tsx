30056d2fa004417350917477a1a8e3d65b9bc205
xsmartbartx <bw141105@gmail.com>
2025-04-06 15:04:51 +0200

    Update GameCard.tsx

--- client/src/components/GameCard.tsx
+++ client/src/components/GameCard.tsx
@@ -2,3 +2,14 @@ import { Button } from "@/components/ui/button";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 import { useLocation } from "wouter";
 import { cn } from "@/lib/utils";
+
+interface GameCardProps {
+    id: number;
+    name: string;
+    description: string;
+    rtp: number;
+    type: string;
+    difficulty: string;
+    popular: boolean;
+    className?: string;
+}
