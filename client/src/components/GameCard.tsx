b4fed2939cebf27d714c549438d5200a8d421159
xsmartbartx <bw141105@gmail.com>
2025-04-06 15:04:40 +0200

    Create GameCard.tsx

---
+++ client/src/components/GameCard.tsx
@@ -0,0 +1,4 @@
+import { Button } from "@/components/ui/button";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { useLocation } from "wouter";
+import { cn } from "@/lib/utils";
