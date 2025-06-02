d5ac85cd3c0f9d0597c682adbdaffb9ce91cfdc0
xsmartbartx <bw141105@gmail.com>
2025-06-02 14:26:00 +0200

    Update use-toast.ts

--- client/src/hooks/use-toast.ts
+++ client/src/hooks/use-toast.ts
@@ -180,3 +180,12 @@ function useToast() {
       }
     }
   }, [state])
+
+    return {
+    ...state,
+    toast,
+    dismiss: (toastId?: string) => dispatch({ type: "DISMISS_TOAST", toastId }),
+  }
+}
+
+export { useToast, toast }
