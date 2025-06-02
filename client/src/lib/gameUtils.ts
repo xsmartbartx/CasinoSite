b66b2213de81b108f60556af42058008ad9fc467
xsmartbartx <bw141105@gmail.com>
2025-06-02 15:30:49 +0200

    Create gameUtils.ts

---
+++ client/src/lib/gameUtils.ts
@@ -0,0 +1,11 @@
+/**
+ * Formats a number as a currency string
+ */
+export function formatCurrency(amount: number): string {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  }).format(amount);
+}
