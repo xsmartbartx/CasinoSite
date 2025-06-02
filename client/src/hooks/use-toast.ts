eb69e0402ac2171c617e8f8e1459ecc127259af5
xsmartbartx <bw141105@gmail.com>
2025-06-02 14:23:03 +0200

    Update use-toast.ts

--- client/src/hooks/use-toast.ts
+++ client/src/hooks/use-toast.ts
@@ -99,3 +99,16 @@ export const reducer = (state: State, action: Action): State => {
           addToRemoveQueue(toast.id)
         })
       }
+
+      return {
+        ...state,
+        toasts: state.toasts.map((t) =>
+          t.id === toastId || toastId === undefined
+            ? {
+                ...t,
+                open: false,
+              }
+            : t
+        ),
+      }
+    }
