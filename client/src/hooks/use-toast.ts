f0f1667bcf89d476c85d1c7fa26a538621ccfdf7
xsmartbartx <bw141105@gmail.com>
2025-06-02 14:20:40 +0200

    Update use-toast.ts

--- client/src/hooks/use-toast.ts
+++ client/src/hooks/use-toast.ts
@@ -28,3 +28,24 @@ function genId() {
   count = (count + 1) % Number.MAX_SAFE_INTEGER
   return count.toString()
 }
+
+type ActionType = typeof actionTypes
+
+type Action =
+  | {
+      type: ActionType["ADD_TOAST"]
+      toast: ToasterToast
+    }
+  | {
+      type: ActionType["UPDATE_TOAST"]
+      toast: Partial<ToasterToast>
+    }
+  | {
+      type: ActionType["DISMISS_TOAST"]
+      toastId?: ToasterToast["id"]
+    }
+  | {
+      type: ActionType["REMOVE_TOAST"]
+      toastId?: ToasterToast["id"]
+    }
+
