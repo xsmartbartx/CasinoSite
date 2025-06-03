5ab4f1fbcb6caba418c08db0cb51724a2574df0d
xsmartbartx <bw141105@gmail.com>
2025-06-03 11:20:03 +0200

    Update Admin.tsx

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -192,3 +192,26 @@ function AdminDashboard() {
           </Card>
         ))}
       </div>
+
+      {/* Activity chart */}
+      <Card className="mt-4">
+        <CardHeader>
+          <CardTitle>Weekly Activity</CardTitle>
+          <CardDescription>
+            Overview of user activity and bets over the past week
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="pl-2">
+          <ResponsiveContainer width="100%" height={350}>
+            <BarChart data={activityData}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis yAxisId="left" orientation="left" stroke="#8884d8" />
+              <YAxis yAxisId="right" orientation="right" stroke="#82ca9d" />
+              <Tooltip />
+              <Bar yAxisId="left" dataKey="users" fill="#8884d8" name="Active Users" />
+              <Bar yAxisId="right" dataKey="bets" fill="#82ca9d" name="Total Bets" />
+            </BarChart>
+          </ResponsiveContainer>
+        </CardContent>
+      </Card>
