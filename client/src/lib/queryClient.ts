b9096e3418c70638a9860276a7ed618fe4fe2c46
xsmartbartx <bw141105@gmail.com>
2025-06-03 11:11:48 +0200

    Create queryClient.ts

---
+++ client/src/lib/queryClient.ts
@@ -0,0 +1,8 @@
+import { QueryClient, QueryFunction } from "@tanstack/react-query";
+
+async function throwIfResNotOk(res: Response) {
+  if (!res.ok) {
+    const text = (await res.text()) || res.statusText;
+    throw new Error(`${res.status}: ${text}`);
+  }
+}
