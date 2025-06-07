7a1099e84a2e245be901fee8ddbcfd998cdf3c4e
xsmartbartx <bw141105@gmail.com>
2025-06-07 18:26:18 +0200

    Create not-found.tsx

---
+++ client/src/pages/not-found.tsx
@@ -0,0 +1,5 @@
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Gamepad2, Home, AlertTriangle, ArrowLeft, Search } from "lucide-react";
+import { Link } from "wouter";
+import { motion } from "framer-motion";
