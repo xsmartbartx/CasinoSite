907a4cecb7a265665ea4804ef1df989a55963304
xsmartbartx <bw141105@gmail.com>
2025-06-09 22:50:57 +0200

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -57,3 +57,12 @@ app.use((req, res, next) => {
     res.status(status).json({ message });
     throw err;
   });
+
+    // importantly only setup vite in development and after
+  // setting up all the other routes so the catch-all route
+  // doesn't interfere with the other routes
+  if (app.get("env") === "development") {
+    await setupVite(app, server);
+  } else {
+    serveStatic(app);
+  }
