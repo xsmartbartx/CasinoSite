08359c8336693c7a8f7026a523c8ca632ccb3808
xsmartbartx <bw141105@gmail.com>
2025-06-09 22:51:09 +0200

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -66,3 +66,15 @@ app.use((req, res, next) => {
   } else {
     serveStatic(app);
   }
+
+    // ALWAYS serve the app on port 5000
+  // this serves both the API and the client
+  const port = 5000;
+  server.listen({
+    port,
+    host: "0.0.0.0",
+    reusePort: true,
+  }, () => {
+    log(`serving on port ${port}`);
+  });
+})();
