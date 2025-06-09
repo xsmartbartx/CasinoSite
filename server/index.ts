90efd92a54876e75f384c5d2dc951dd032f599de
xsmartbartx <bw141105@gmail.com>
2025-06-09 21:59:47 +0200

    Create index.ts

---
+++ server/index.ts
@@ -0,0 +1,4 @@
+import express, { type Request, Response, NextFunction } from "express";
+import { registerRoutes } from "./routes";
+import { setupVite, serveStatic, log } from "./vite";
+import { storage } from "./storage";
