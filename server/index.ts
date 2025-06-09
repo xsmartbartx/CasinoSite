45c4b5dd59f13f45ea10a4556c05acc949744c51
xsmartbartx <bw141105@gmail.com>
2025-06-09 22:50:44 +0200

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -47,3 +47,13 @@ app.use((req, res, next) => {
       console.error("Error seeding database:", error);
     }
   }
+
+    const server = await registerRoutes(app);
+
+  app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
+    const status = err.status || err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+
+    res.status(status).json({ message });
+    throw err;
+  });
