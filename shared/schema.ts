274f01b72879e267ce8d9f9215eb70253bc7e9ff
xsmartbartx <bw141105@gmail.com>
2025-06-16 16:07:45 +0200

    Create schema.ts

---
+++ shared/schema.ts
@@ -0,0 +1,3 @@
+import { pgTable, text, serial, integer, boolean, timestamp, doublePrecision, pgEnum } from "drizzle-orm/pg-core";
+import { createInsertSchema } from "drizzle-zod";
+import { z } from "zod";
