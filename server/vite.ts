4574aee9f917053bf280970cfaea52283b457858
xsmartbartx <bw141105@gmail.com>
2025-06-16 16:04:16 +0200

    Create vite.ts

---
+++ server/vite.ts
@@ -0,0 +1,10 @@
+import express, { type Express } from "express";
+import fs from "fs";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import { createServer as createViteServer, createLogger } from "vite";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+import { type Server } from "http";
+import viteConfig from "../vite.config";
+import { nanoid } from "nanoid";
