da0287fd6aa770e8d7efc540a67e265a08cc6999
xsmartbartx <bw141105@gmail.com>
2025-06-16 16:05:32 +0200

    Update vite.ts

--- server/vite.ts
+++ server/vite.ts
@@ -9,3 +9,12 @@ import { type Server } from "http";
 import viteConfig from "../vite.config";
 import { nanoid } from "nanoid";
 
+const viteLogger = createLogger();
+
+export function log(message: string, source = "express") {
+  const formattedTime = new Date().toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  });
