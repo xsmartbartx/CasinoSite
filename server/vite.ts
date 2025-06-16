7a927752379851244f8762ca7789e80b720bb69e
xsmartbartx <bw141105@gmail.com>
2025-06-16 16:06:36 +0200

    Update vite.ts

--- server/vite.ts
+++ server/vite.ts
@@ -69,3 +69,20 @@ export async function setupVite(app: Express, server: Server) {
     }
   });
 }
+
+export function serveStatic(app: Express) {
+  const distPath = path.resolve(__dirname, "public");
+
+  if (!fs.existsSync(distPath)) {
+    throw new Error(
+      `Could not find the build directory: ${distPath}, make sure to build the client first`,
+    );
+  }
+
+  app.use(express.static(distPath));
+
+  // fall through to index.html if the file doesn't exist
+  app.use("*", (_req, res) => {
+    res.sendFile(path.resolve(distPath, "index.html"));
+  });
+}
